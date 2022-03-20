442177f6646d260d4a078786f9c4035220077ceb
Damini <94996554+DaminiTiwari@users.noreply.github.com>
2022-03-20 23:17:57 +0530

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,1 +1,10 @@
 
+import { configureStore } from '@reduxjs/toolkit';
+import projectReducer from '../features/projectSlice';
+
+export const store = configureStore({
+  reducer: {
+    projects: projectReducer,
+  },
+  devTools: false,
+});
