c31b96ab6d8cd1b0236fc621c07dce1ae4e63eaf
Damini <94996554+DaminiTiwari@users.noreply.github.com>
2022-03-22 07:44:19 +0530

    keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,25 +2,20 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth"
 import "firebase/compat/firestore"
 import { initializeApp } from "firebase/app";
-// import "firebase/storage";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC1v-WvTtnCmyLs_njECcehlX9Vks133N8",
-  authDomain: "daminit1.firebaseapp.com",
-  projectId: "daminit1",
-  storageBucket: "daminit1.appspot.com",
-  messagingSenderId: "37979057522",
-  appId: "1:37979057522:web:38ad5ce6ecee80542a41eb"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
 const app = firebase.initializeApp(firebaseConfig);
-const db = app.firestore(); // to connect with reacltime firebase database
-
-const auth = firebase.auth(); //connect authentication
+const db = app.firestore();
 
 
-const provider = new firebase.auth.GoogleAuthProvider();
-// export const storage = firebase.storage();
-
-export  {db, auth, provider};
+export  {db};
